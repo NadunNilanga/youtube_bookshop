492a418c6ef6f21171e11324acf6cf9462ac0e5b
NadunNilanga <54107433+NadunNilanga@users.noreply.github.com>
2020-11-18 02:20:10 +0530

    clock

--- view/home/home.php
+++ view/home/home.php
@@ -25,6 +25,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <link rel="stylesheet" href="../../Assets/AdminLte3/dist/css/adminlte.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+    <style>
+        @media only screen and (max-width: 800px) {
+
+            #displayDateTime {
+                display: none;
+            }
+
+            #canvas {
+                display: none;
+            }
+        }
+    </style>
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -161,41 +174,35 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <div class="content-header">
                 <div class="container-fluid">
                     <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0 text-dark"> <span class="nav-link text-info" id="displayDateTime">2020-11-18 </span></h1>
-                        </div><!-- /.col -->
-                        <div class="col-sm-6">
+                        <!-- Left Content -->
+                        <div class="col-sm-9">
+                            <!-- Main content -->
+                            <div class="content">
+                                <div class="container-fluid">
+                                    <div class="row">
 
-                        </div><!-- /.col -->
+                                    </div>
+                                    <!-- /.row -->
+                                </div><!-- /.container-fluid -->
+                            </div>
+                            <!-- /.content -->
+
+                        </div><!-- /.col   Left Content -->
+                        <!-- clock -->
+                        <div class="col-sm-3 text-right">
+                            <canvas id="canvas" width="160" height="160"> </canvas>
+                            <h1 class="m-0 text-dark"> <span class="nav-link text-info" id="displayDateTime">2020-11-18 </span></h1>
+                        </div><!-- /.col clock-->
                     </div><!-- /.row -->
                 </div><!-- /.container-fluid -->
             </div>
             <!-- /.content-header -->
 
-            <!-- Main content -->
-            <div class="content">
-                <div class="container-fluid">
-                    <div class="row">
 
-
-
-                    </div>
-                    <!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </div>
-            <!-- /.content -->
         </div>
         <!-- /.content-wrapper -->
 
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-            <div class="p-3">
-                <h5>Title</h5>
-                <p>Sidebar content</p>
-            </div>
-        </aside>
-        <!-- /.control-sidebar -->
+
 
         <!-- Main Footer -->
         <footer class="main-footer">
@@ -219,23 +226,99 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="../../Assets/AdminLte3/dist/js/adminlte.min.js"></script>
 
     <script>
-        startTime();
+        showDate();
 
-        function startTime() {
+        function showDate() {
             var today = new Date();
             var year = today.getFullYear();
             var month = today.getMonth();
             var date = today.getDate();
+            document.getElementById('displayDateTime').innerHTML = year + "-" + month + "-" + date;
 
-            document.getElementById('displayDateTime').innerHTML = year + "-" + month + "-" + date + " " + today.toLocaleTimeString();
-            var t = setTimeout(startTime, 500);
         }
 
-        function checkTime(i) {
-            if (i < 10) {
-                i = "0" + i
-            }; // add zero in front of numbers < 10
-            return i;
+
+
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+        var radius = canvas.height / 2;
+        ctx.translate(radius, radius);
+        radius = radius * 0.90
+        setInterval(drawClock, 1000);
+
+        function drawClock() {
+            ctx.clearRect(-radius, -radius, canvas.width, canvas.height);
+            drawFace(ctx, radius);
+            drawNumbers(ctx, radius);
+            drawTime(ctx, radius);
+
+        }
+
+        function drawFace(ctx, radius) {
+            var grad;
+            ctx.beginPath();
+            /*  ctx.arc(0, 0, radius, 0, 2*Math.PI);
+              ctx.fillStyle = 'white';
+              ctx.fill();*/
+            grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+            grad.addColorStop(0, '#333');
+            grad.addColorStop(0.5, 'white');
+            grad.addColorStop(1, '#333');
+            ctx.strokeStyle = grad;
+            ctx.lineWidth = radius * 0.1;
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+            ctx.fillStyle = '#333';
+            ctx.fill();
+        }
+
+        function drawNumbers(ctx, radius) {
+            var ang;
+            var num;
+            ctx.font = radius * 0.15 + "px arial";
+            ctx.textBaseline = "middle";
+            ctx.textAlign = "center";
+            for (num = 1; num < 13; num++) {
+                ang = num * Math.PI / 6;
+                ctx.rotate(ang);
+                ctx.translate(0, -radius * 0.85);
+                ctx.rotate(-ang);
+                ctx.fillText(num.toString(), 0, 0);
+                ctx.rotate(ang);
+                ctx.translate(0, radius * 0.85);
+                ctx.rotate(-ang);
+            }
+        }
+
+        function drawTime(ctx, radius) {
+            var now = new Date();
+            var hour = now.getHours();
+            var minute = now.getMinutes();
+            var second = now.getSeconds();
+            //hour
+            hour = hour % 12;
+            hour = (hour * Math.PI / 6) +
+                (minute * Math.PI / (6 * 60)) +
+                (second * Math.PI / (360 * 60));
+            drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+            //minute
+            minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+            drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+            // second
+            second = (second * Math.PI / 30);
+            drawHand(ctx, second, radius * 0.9, radius * 0.02);
+        }
+
+        function drawHand(ctx, pos, length, width) {
+            ctx.beginPath();
+            ctx.lineWidth = width;
+            ctx.lineCap = "round";
+            ctx.moveTo(0, 0);
+            ctx.rotate(pos);
+            ctx.lineTo(0, -length);
+            ctx.stroke();
+            ctx.rotate(-pos);
         }
     </script>
 </body>
